b002edafe2ad88dec675b1d6881417f9807cd57c
Jean claude Garcia <garciajeanclaude3773@gmail.com>
2025-09-16 11:46:40 +0200

    Remplissage des tables

--- tifosi.sql
+++ tifosi.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : ven. 12 sep. 2025 à 17:44
+-- Généré le : mar. 16 sep. 2025 à 11:39
 -- Version du serveur : 10.4.32-MariaDB
 -- Version de PHP : 8.2.12
 
@@ -29,8 +29,27 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `boisson` (
   `id_boisson` int(11) NOT NULL,
-  `nom` varchar(50) NOT NULL
+  `nom_boisson` varchar(50) NOT NULL,
+  `marque` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+--
+-- Déchargement des données de la table `boisson`
+--
+
+INSERT INTO `boisson` (`id_boisson`, `nom_boisson`, `marque`) VALUES
+(1, 'Coca-cola zéro', 'Coca-cola'),
+(2, 'Coca-cola original', 'Coca-cola'),
+(3, 'Fanta citron', 'Coca-cola'),
+(4, 'Fanta orange', 'Coca-cola'),
+(5, 'Capri-sun', 'Coca-cola'),
+(6, 'Pepsi', 'Pepsico'),
+(7, 'Pepsi Max zéro', 'Pepsico'),
+(8, 'Lipton zéro citron', 'Pepsico'),
+(9, 'Lipton Peach', 'Pepsico'),
+(10, 'Monster energy ultra gold', 'Monster'),
+(11, 'Monster energy ultra blue', 'Monster'),
+(12, 'Eau de source', 'Cristalline');
 
 -- --------------------------------------------------------
 
@@ -40,7 +59,7 @@ CREATE TABLE `boisson` (
 
 CREATE TABLE `client` (
   `id_client` int(11) NOT NULL,
-  `nom` varchar(50) NOT NULL,
+  `nom_client` varchar(50) NOT NULL,
   `email` varchar(150) NOT NULL,
   `code_postal` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
@@ -53,9 +72,23 @@ CREATE TABLE `client` (
 
 CREATE TABLE `foccacia` (
   `id_foccacia` int(11) NOT NULL,
-  `nom` varchar(50) NOT NULL,
+  `nom_foccacia` varchar(50) NOT NULL,
   `prix` decimal(5,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+--
+-- Déchargement des données de la table `foccacia`
+--
+
+INSERT INTO `foccacia` (`id_foccacia`, `nom_foccacia`, `prix`) VALUES
+(1, 'Mozaccia', 9.80),
+(2, 'Gorgonzollaccia', 10.80),
+(3, 'Raclaccia', 8.90),
+(4, 'Emmentalaccia', 9.80),
+(5, 'Tradizione', 8.90),
+(6, 'Hawaienne', 11.20),
+(7, 'Américaine', 10.80),
+(8, 'Paysanne', 12.80);
 
 -- --------------------------------------------------------
 
@@ -65,8 +98,39 @@ CREATE TABLE `foccacia` (
 
 CREATE TABLE `ingredient` (
   `id_ingredient` int(11) NOT NULL,
-  `nom` varchar(50) NOT NULL
+  `nom_ingredient` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+--
+-- Déchargement des données de la table `ingredient`
+--
+
+INSERT INTO `ingredient` (`id_ingredient`, `nom_ingredient`) VALUES
+(1, 'Ail'),
+(2, 'Ananas'),
+(3, 'Artichaut'),
+(4, 'Bacon'),
+(5, 'Base Tomate'),
+(6, 'Base crème'),
+(7, 'Champignon'),
+(8, 'Chevre'),
+(9, 'Cresson'),
+(10, 'Emmental'),
+(11, 'Gorgonzola'),
+(12, 'Jambon cuit'),
+(13, 'Jambon fumé'),
+(14, 'Oeuf'),
+(15, 'Oignon'),
+(16, 'Olive noire'),
+(17, 'Olive verte'),
+(18, 'Parmesan'),
+(19, 'Piment'),
+(20, 'Poivre'),
+(21, 'Pomme de terre'),
+(22, 'Raclette'),
+(23, 'Salami'),
+(24, 'Tomate cerise'),
+(25, 'Mozarella');
 
 -- --------------------------------------------------------
 
@@ -76,8 +140,18 @@ CREATE TABLE `ingredient` (
 
 CREATE TABLE `marque` (
   `id_marque` int(11) NOT NULL,
-  `nom` varchar(50) NOT NULL
+  `nom_marque` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+--
+-- Déchargement des données de la table `marque`
+--
+
+INSERT INTO `marque` (`id_marque`, `nom_marque`) VALUES
+(1, 'Coca-cola'),
+(2, 'Cristalline'),
+(3, 'Monster'),
+(4, 'Pepsico');
 
 -- --------------------------------------------------------
 
@@ -139,7 +213,7 @@ ALTER TABLE `menu`
 -- AUTO_INCREMENT pour la table `boisson`
 --
 ALTER TABLE `boisson`
-  MODIFY `id_boisson` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_boisson` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT pour la table `client`
@@ -151,19 +225,19 @@ ALTER TABLE `client`
 -- AUTO_INCREMENT pour la table `foccacia`
 --
 ALTER TABLE `foccacia`
-  MODIFY `id_foccacia` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_foccacia` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT pour la table `ingredient`
 --
 ALTER TABLE `ingredient`
-  MODIFY `id_ingredient` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_ingredient` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT pour la table `marque`
 --
 ALTER TABLE `marque`
-  MODIFY `id_marque` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_marque` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT pour la table `menu`
